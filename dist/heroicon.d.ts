b2bc3db61350f3fb5e0998dbf8aced6dc4f107d4
justinvoitel <justin.voitel@gmail.com>
2021-07-29 20:22:12 +0200

    * exclude dist

--- dist/heroicon.d.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement } from "lit-element";
-import { HeroIconName } from "./generated/heroicons";
-export declare class Heroicon extends LitElement {
-    solid: boolean;
-    name: HeroIconName;
-    render(): import("lit-element").TemplateResult;
-    createRenderRoot(): this;
-}
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./generated/heroicons";
-export * from "./heroicon";
